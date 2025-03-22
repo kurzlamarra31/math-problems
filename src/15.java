c1085416dbb292d57ae3f2edcc471f6ceaa62f7b
kurzlamarra31 <kurzlamarra31@gmail.com>
2025-03-22 16:20:47 +0000

    Add 15.java

---
+++ src/15.java
@@ -0,0 +1,9 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        // Example program with a simple addition problem
+        int num1 = 5;
+        int num2 = 3;
+        int sum = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
