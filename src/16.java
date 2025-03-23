6fa219844c150e99be00ff96856a024567fdfa18
kurzlamarra31 <kurzlamarra31@gmail.com>
2025-03-23 10:53:11 +0000

    Add 16.java

---
+++ src/16.java
@@ -0,0 +1,21 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        // Example 1: Add two numbers
+        int num1 = 5;
+        int num2 = 3;
+        int sum = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+
+        // Example 2: Subtract two numbers
+        double result = (double) 5.0 - (double) 3.0;
+        System.out.println("The result of 5.0 minus 3.0 is: " + result);
+
+        // Example 3: Multiply two numbers
+        int product = 10 * 2;
+        System.out.println("The product of 10 and 2 is: " + product);
+
+        // Example 4: Divide two numbers
+        double quotient = (double) 5.0 / (double) 3.0;
+        System.out.println("The quotient of 5.0 divided by 3.0 is: " + quotient);
+    }
+}
