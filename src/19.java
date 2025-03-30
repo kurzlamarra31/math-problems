88c9c9c56d6e5df75c46876f3b859be6b423bee9
kurzlamarra31 <kurzlamarra31@gmail.com>
2025-03-30 01:13:24 +0000

    Add 19.java

---
+++ src/19.java
@@ -0,0 +1,23 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        // Example problem 1: Sum of two numbers
+        int sum = 3 + 5;
+        System.out.println("The sum is: " + sum);
+
+        // Example problem 2: Multiplication of three numbers
+        double product = 4 * 6 * 8;
+        System.out.println("The product is: " + product);
+
+        // Example problem 3: Area of a rectangle
+        int width = 10;
+        int height = 5;
+        double area = width * height;
+        System.out.println("The area is: " + area);
+
+        // Example problem 4: Division of two numbers
+        int dividend = 8;
+        int divisor = 2;
+        double quotient = dividend / divisor;
+        System.out.println("The quotient is: " + quotient);
+    }
+}
