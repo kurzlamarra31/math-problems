5b6e88728b4b019cbbf77f3c1992d74904e0af0c
kurzlamarra31 <kurzlamarra31@gmail.com>
2025-04-08 05:29:01 +0000

    Add 26.java

---
+++ src/26.java
@@ -0,0 +1,13 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        // Example 1: Adding two numbers
+        int num1 = 5;
+        int num2 = 3;
+        System.out.println("The sum is: " + (num1 + num2));
+        
+        // Example 2: Multiplying two numbers
+        double num1Double = 7.0;
+        double num2Double = 4.0;
+        System.out.println("The product is: " + (num1Double * num2Double));
+    }
+}
