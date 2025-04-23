1d39d686421503aecf14cd7fffacc70c2452516f
kurzlamarra31 <kurzlamarra31@gmail.com>
2025-04-23 11:13:23 +0000

    Add 29.java

---
+++ src/29.java
@@ -0,0 +1,19 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        // Example 1: Adding two numbers
+        int num1 = 5;
+        int num2 = 3;
+        System.out.println("Sum of " + num1 + " and " + num2 + " is: " + (num1 + num2));
+        
+        // Example 2: Finding the area of a rectangle
+        double length = 8.0;
+        double width = 4.5;
+        double area = length * width;
+        System.out.println("The area of the rectangle with length=" + length + " and width=" + width + " is: " + area);
+        
+        // Example 3: Calculating the circumference of a circle
+        double radius = 7.0;
+        double circumference = 2 * Math.PI * radius;
+        System.out.println("The circumference of the circle with radius=" + radius + " is: " + circumference);
+    }
+}
