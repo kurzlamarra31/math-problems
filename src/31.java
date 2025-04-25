ed35e0f2608de0f30b369ec52c212dfe773e4b41
kurzlamarra31 <kurzlamarra31@gmail.com>
2025-04-25 09:31:50 +0000

    Add 31.java

---
+++ src/31.java
@@ -0,0 +1,17 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        // Example Java code for mathematical calculations
+
+        double sum = 0;
+        double difference = 10.5;
+
+        sum += difference;
+        System.out.println("The sum of " + difference + " and 10.5 is: " + sum);
+
+        int product = 2 * 3;
+        long quotient = 4 / 2;
+
+        System.out.println("The product of 2 and 3, which equals to " + product);
+        System.out.println("Quotient of 4 divided by 2, which equals to " + quotient);
+    }
+}
