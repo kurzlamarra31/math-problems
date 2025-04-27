6ad7f96c252a6bc7773c83ab673e72350bc0d5c1
kurzlamarra31 <kurzlamarra31@gmail.com>
2025-04-27 02:19:21 +0000

    Add 32.java

---
+++ src/32.java
@@ -0,0 +1,14 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        // Example Java program to solve mathematical problems
+
+        System.out.println("Hello World!");
+        
+        double a = 3.0;
+        double b = 5.0;
+        double c = 7.0;
+
+        double result = (a + b) * c;
+        System.out.println(result);
+    }
+}
