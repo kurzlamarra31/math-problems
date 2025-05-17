3bb03d540df5f3b3aaf1f8e21301d1145c95c0e4
kurzlamarra31 <kurzlamarra31@gmail.com>
2025-05-17 04:10:54 +0000

    Add 33.java

---
+++ src/33.java
@@ -0,0 +1,7 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        // Example mathematical problem: 5 + 3 * 2 - (4 / 2)
+        double result = 5 + 3 * 2 - (4 / 2);
+        System.out.println("Result of the math problem is: " + result);
+    }
+}
