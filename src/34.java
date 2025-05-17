a82501a749e1e02f96be6476e7c8fe7a96db7a6b
kurzlamarra31 <kurzlamarra31@gmail.com>
2025-05-17 07:03:13 +0000

    Add 34.java

---
+++ src/34.java
@@ -0,0 +1,8 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        System.out.println("Hello, this is a Java program.");
+        int i = 5;
+        double pi = 3.14159265358979323846;
+        String name = "Alice";
+    }
+}
