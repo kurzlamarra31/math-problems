8062dc3143e5f7b3bc065e395ff0220fc69eec47
kurzlamarra31 <kurzlamarra31@gmail.com>
2025-05-18 22:24:50 +0000

    Add 35.java

---
+++ src/35.java
@@ -0,0 +1,17 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        int num1 = 3;
+        int num2 = 5;
+
+        // Perform some arithmetic operations
+        int sum = num1 + num2;
+        int multiplication = num1 * num2;
+        int subtraction = num1 - num2;
+        int division = num1 / num2;
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Multiplication: " + multiplication);
+        System.out.println("Subtraction: " + subtraction);
+        System.out.println("Division: " + division);
+    }
+}
