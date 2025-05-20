3c8c5b7aba3630d0bb0858739f952868b62e6287
kurzlamarra31 <kurzlamarra31@gmail.com>
2025-05-20 07:43:50 +0000

    Add 37.java

---
+++ src/37.java
@@ -0,0 +1,10 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        int x = 5;
+        double y = 3.14;
+        System.out.println("x + y: " + (x + y));
+        System.out.println("x - y: " + (x - y));
+        System.out.println("x * y: " + (x * y));
+        System.out.println("x / y: " + (x / y));
+    }
+}
