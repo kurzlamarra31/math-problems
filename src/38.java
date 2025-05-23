ae40e9c5c7f43b1f7db29cdf8941150d76127448
kurzlamarra31 <kurzlamarra31@gmail.com>
2025-05-23 06:56:57 +0000

    Add 38.java

---
+++ src/38.java
@@ -0,0 +1,11 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 3;
+        
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + (num1 + num2));
+        
+        double result = Math.sqrt(num1 * num1);
+        System.out.println("The square root of " + num1 + " squared is: " + result);
+    }
+}
