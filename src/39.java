909e7dc01d446ee7cb51946b41409ee421840fac
kurzlamarra31 <kurzlamarra31@gmail.com>
2025-05-24 04:19:52 +0000

    Add 39.java

---
+++ src/39.java
@@ -0,0 +1,9 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        // Example of solving a simple math problem: 2 + 3 = 5
+        System.out.println("The answer is 5.");
+        
+        // Another example of solving an arithmetic expression: (4 * 2) - 10 = 6
+        System.out.println("(4 * 2) - 10 = 6");
+    }
+}
