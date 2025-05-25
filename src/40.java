d2af820048bf72fd741dd4d6f3d27af6932e4500
kurzlamarra31 <kurzlamarra31@gmail.com>
2025-05-25 22:27:04 +0000

    Add 40.java

---
+++ src/40.java
@@ -0,0 +1,18 @@
+public class MathProblems {
+    public static void main(String[] args) {
+        // Example Java program that demonstrates basic mathematical operations
+        System.out.println("This is a simple calculator program.");
+        
+        int num1 = 5;
+        int num2 = 3;
+        double sum = num1 + num2;
+        double difference = num1 - num2;
+        double product = num1 * num2;
+        double quotient = num1 / num2;
+
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+        System.out.println("The difference between " + num1 + " and " + num2 + " is: " + difference);
+        System.out.println("The product of " + num1 + " and " + num2 + " is: " + product);
+        System.out.println("The quotient of " + num1 + " and " + num2 + " is: " + quotient);
+    }
+}
